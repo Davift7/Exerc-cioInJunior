b960111dd2373f89ea58c9e6328aef6b4721c9db
Davi Tavares Fernandes <daviftavares777@gmail.com>
2025-06-29 01:23:50 -0300

    Exercício 8

---
+++ Exercício8.js
@@ -0,0 +1,100 @@
+const booksByCategory = [
+    {
+        category: "Romance",
+        books: [
+            {
+                title: "O Sol Também é Uma Estrela",
+                author: "Nicola Yoon",
+            },
+            {
+                title: "O Noivo da Minha Melhor Amiga",
+                author: "Emily Gin",
+            },
+            {
+                title: "Orgulho e Preconceito",
+                author: "Jane Austen",
+            },
+        ],
+    },
+    {
+        category: "Fantasia",
+        books: [
+            {
+                title: "Harry Potter e a Pedra Filosofal",
+                author: "J.K. Rowling",
+            },
+            {
+                title: "O Hobbit",
+                author: "J.R.R. Tolkien",
+            },
+            {
+                title: "Eragon",
+                author: "Christopher Paolini",
+            },
+            {
+                title: "O Nome do Vento",
+                author: "Patrick Rothfuss",
+            },
+        ],
+    },
+    {
+        category: "Suspense",
+        books: [
+            {
+                title: "A Garota no Trem",
+                author: "Paula Hawkins",
+            },
+            {
+                title: "O Silêncio dos Inocentes",
+                author: "Thomas Harris",
+            },
+        ],
+    },
+    {
+        category: "Ficção Científica",
+        books: [
+            {
+                title: "Duna",
+                author: "Frank Herbert",
+            },
+            {
+                title: "Neuromancer",
+                author: "William Gibson",
+            },
+            {
+                title: "Fundação",
+                author: "Isaac Asimov",
+            },
+            {
+                title: "1984",
+                author: "George Orwell",
+            },
+            {
+                title: "O Fim da Infância",
+                author: "Arthur C. Clarke",
+            },
+        ],
+    },
+];
+
+
+
+let tam = booksByCategory.length
+
+
+function countBooksInCategory(tam, array) {
+    let livraria = {};
+    for (let i = 0; i < tam; i++) {
+        let titulo = array[i].category
+        let quantidade = array[i].books.length
+
+        livraria[titulo] = quantidade
+
+    }
+    return livraria;
+}
+
+
+
+
+
